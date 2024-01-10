9c4adc70a23d157ae3a96aac2b1ab64790bfb1c4
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-01-10 21:43:19 +0530

    JDBC

--- tourpacakage/MyTourPackage.java
+++ tourpacakage/MyTourPackage.java
@@ -8,17 +8,18 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import utility.JdbcUtil;
 
 public class MyTourPackage {
-	static List<Package> addPackageData(){
+	public static List<Package> addPackageData(){
 		List<Package> list = new ArrayList();
-		try {
-			Connection con = JdbcUtil.getConnectionWithDB();
-			PreparedStatement ps = con.prepareStatement("insert into train_table values(?, ?, ?, ?, ?)");
+		try (Connection con = JdbcUtil.getConnectionWithDB();){
+			String query = "insert into train_table values(?, ?, ?, ?, ?)";
+			PreparedStatement ps = con.prepareStatement(query);
 			ResultSet rs = ps.executeQuery();
 			while(rs.next()) {
 				String pID=rs.getString(1);
@@ -26,8 +27,8 @@ public class MyTourPackage {
 				String dest = rs.getString(3);
 				int nDay = rs.getInt(4);
 				Double cost = rs.getDouble(5);
-				Package tObj = new Package(pID, source, dest, nDay, cost);
-				list.add(tObj);
+//				Package tObj = new Package(pID, source, dest, nDay, cost);
+//				list.add(tObj);
 			} 
 		} 
 		catch(Exception e) {
@@ -36,6 +37,10 @@ public class MyTourPackage {
 		return list;
 	}
 	public static void main(String[] args) throws NumberFormatException, IOException {
+		try (Connection con = JdbcUtil.getConnectionWithDB();){
+		String query = "insert into package_details values(?, ?, ?, ?, ?)";
+		PreparedStatement ps = con.prepareStatement(query);
+
 		File file = new File("./src/TourPackage.txt");
 		Reader reader = new FileReader(file);
 		BufferedReader br = new BufferedReader(reader);
@@ -49,12 +54,22 @@ public class MyTourPackage {
 			String dest = str[2];
 			int nDay = Integer.parseInt(str[3]);
 			double cost = Double.parseDouble(str[4]);
+			
 			Package pack = new  Package(pID, source, dest, nDay, cost);
 			System.out.println(pack.getPackageId()+" "+pack.getSource()+" "+pack.getDestination()+" "+pack.getNod()+" "+pack.getPackageCost());
-			List<Package> resultlist = addPackageData();
+			list.add(pack);
+			
+			 ps.setString(1, pack.getPackageId());
+             ps.setString(2, pack.getSource());
+             ps.setString(3, pack.getDestination());
+             ps.setInt(4, pack.getNod());
+             ps.setDouble(5, pack.getPackageCost());
+             ps.executeUpdate();
+			
 		}
-//		List<Package> resultlist = addPackageData();
-//		resultlist.forEach(t1->System.out.println(t1.getPackageId()+" "+t1.getSource()+" "+t1.getDestination()+" "+t1.getNod()+" "+t1.getPackageCost()));
-		
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+//		addPackageData();		
 	}
 }
