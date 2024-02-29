4ec9c690bdcf0285377e4a56dc5438ff6045e184
gauravk0247 <gauravkhairnar2106@gmail.com>
2024-02-29 19:18:31 +0530

    JDBC

--- practice/Practice.java
+++ practice/Practice.java
@@ -1,25 +1,49 @@
 package practice;
 
-import java.util.Arrays;
+import java.util.Scanner;
 
-public class Practice{
-	public static void main(String[] args){
-		String str = "Hii How are you";
-		int count = 0;
-		
-		for(char ch : str.toCharArray()) {
-			if(ch=='i') {
-				count++;
-			}
-		}
-		System.out.println(count);
+enum UserStatus{
+	ACTIVATED, DEACTIVATED;
+}
+class User{
+	private String userName;
+	private String userPassword;
+	private UserStatus status;
+	public String getUserName() {
+		return userName;
 	}
-//	public static void getCountOccurance() {
-//		for(int i=0;i<str.length();i++) {
-//			if(str.charAt(i)== val) {
-//				count++;
-//			}
-//			System.out.println(val + " "+ count);
-//		}
-//	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+	public UserStatus getStatus() {
+		return status;
+	}
+	public void setStatus(UserStatus status) {
+		this.status = status;
+	}
+}
+public class Practice{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter User Name");
+		String uname = sc.next();
+		System.out.println("Enter User Password");
+		String upwd = sc.next();
+		System.out.println("Enter User Status either Activated or Deactivated");
+		String status1 = sc.next();
+		
+		UserStatus status2 = UserStatus.valueOf(status1);
+		
+		User user = new User();
+		user.setUserName(uname);
+		user.setUserPassword(upwd);
+		user.setStatus(status2);
+		System.out.println(user.getUserName()+" "+ user.getUserPassword()+" "+ user.getStatus());
+	}
 }
